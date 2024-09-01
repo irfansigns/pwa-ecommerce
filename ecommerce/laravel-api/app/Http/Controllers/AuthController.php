885e523f170b6a0a86853b5a41b4a3f6c6a5430c
UbuntuLaptop <irfansigns@gmail.com>
2024-09-01 12:24:33 +0500

    Backend Setup

--- ecommerce/laravel-api/app/Http/Controllers/AuthController.php
+++ ecommerce/laravel-api/app/Http/Controllers/AuthController.php
@@ -10,6 +10,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        // return "Register";
         $fields = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
---
+++ ecommerce/laravel-api/reactJs/src/router.jsx
@@ -0,0 +1,36 @@
+import {createBrowserRouter} from 'react-router-dom';
+import Login from './views/login.jsx';
+import Register from './views/register.jsx';
+import DefaultLayout from './Components/DefaultLayout.jsx';
+import GuestLayout from './Components/GuestLayout.jsx';
+import Users from './views/users.jsx';
+
+const router = createBrowserRouter ([
+    // array of objects. here objects are routes. element name shouldn't be started with small letter
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/users',
+                element: <Users />,
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/register',
+                element:  <Register />,
+            }
+        ]
+    },
+]);
+
+export default router;
