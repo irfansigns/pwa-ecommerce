5f029f461ca41b81ede7ec2d17988e10cc49f325
irfansigns <irfansigns@gmail.com>
2024-09-08 17:49:21 +0500

    Flow

--- ecommerce/laravel-api/reactJs/src/context/productReducer.jsx
+++ ecommerce/laravel-api/reactJs/src/context/productReducer.jsx
@@ -14,9 +14,20 @@ export const ProductReducer = (state, action) => {
                     ...state,
                     products: [...state.products, ...newProducts]
                 };
-            // Handle other actions
+                // 'products array' of ...state(copied state) is assigned a new value which is updated array named '...newProducts' which is the copy of 'newProducts'
+            
+                // Handle other actions
             default:
                 return state;
         }
     };
+
+    // ABOVE FUNCTION CAN ALSO BE WRITTEN AS
+
+    // const newProducts = action.payload.filter(function(np) {
+    //     return !state.products.some(function(p) {
+    //       return p.id === np.id;
+    //     });
+    //   });
+      
     
